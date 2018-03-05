174969a5fd53181c76953a03f8a0c8f24ba99308
ruirui.qu <ruirui.qu@qunar.com>
2018-03-05 12:15:01 +0800

    add atmoic

--- javacode/src/main/java/artConcurrent/book/thread/threadjoin/Join.java
+++ javacode/src/main/java/artConcurrent/book/thread/threadjoin/Join.java
@@ -2,6 +2,27 @@ package artConcurrent.book.thread.threadjoin;
 
 // --------------------- Change Logs----------------------
 // <p>@author ruirui.qu Initial Created at 17/9/2<p>
+// 当前线程执行，必须从上一个线程结束，才执行，0线程必须在main函数结束后才执行
+/*      main函数结束
+        0is treminate
+
+        1is treminate
+
+        2is treminate
+
+        3is treminate
+
+        4is treminate
+
+        5is treminate
+
+        6is treminate
+
+        7is treminate
+
+        8is treminate
+
+        9is treminate*/
 // -------------------------------------------------------
 public class Join {
 
@@ -15,6 +36,7 @@ public class Join {
 
             previous = thread;
         }
+        System.out.println("main函数结束");
     }
 
     // --------------------- Change Logs----------------------
@@ -34,7 +56,7 @@ public class Join {
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
-            System.out.printf(Thread.currentThread().getName() + "is treminate\n");
+            System.out.println(Thread.currentThread().getName() + "is treminate\n");
         }
     }
 }
