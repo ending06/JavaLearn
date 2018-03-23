eaf20cb8ea344db96609a52059ad6b0c21c9141d
ruirui.qu <ruirui.qu@qunar.com>
2018-03-23 15:26:20 +0800

    add

---
+++ javacode/src/main/java/suanfa/prc/rabbit.java
@@ -0,0 +1,34 @@
+package suanfa.prc;
+
+
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
+// --------------------- Change Logs----------------------
+// <p>@author ruirui.qu Initial Created at 18/3/223<p>
+// -------------------------------------------------------
+public class Rabbit {
+
+    private static final String INT_PATTERN = "[0-9]*";
+
+    public static void main(String[] args) throws Exception {
+        Scanner scanner = new Scanner(System.in);
+
+        String num = scanner.nextLine();
+
+        Pattern pattern = Pattern.compile(INT_PATTERN);
+
+        if(!pattern.matcher(num).matches()){
+            throw new Exception("输入字符非法");
+        }
+
+        System.out.printf("第num=" + num + "的个数为：" + function(Integer.parseInt(num)));
+    }
+
+    private static int function(int num) {
+        if (num==1||num==2) {
+            return 1;
+        }
+        return function(num-1)+function(num-2);
+    }
+}
