0cc80a32193234da583149670d24a8551c193003
ruirui.qu <ruirui.qu@qunar.com>
2018-06-13 17:54:19 +0800

    add

---
+++ javacode/src/main/java/interview/suanfa/prac/KNumZero.java
@@ -0,0 +1,20 @@
+package interview.suanfa.prac;
+
+// --------------------- Change Logs----------------------
+// <p>@author ruirui.qu Initial Created at 18/6/13<p>
+// =====================delK个连续0=======================
+// -------------------------------------------------------
+public class KNumZero {
+
+    public static void main(String[] args) {
+        int k = 3;
+        String inputStr = "A0000B000C";
+
+        String result = removeKZeros(inputStr, k);
+        System.out.println("delK个连续0==>" + result);
+    }
+
+    public static String removeKZeros(String str, int k) {
+        return null;
+    }
+}
