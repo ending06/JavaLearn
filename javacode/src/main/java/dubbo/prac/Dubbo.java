4863553f53e1adcb178bf81a4698477d9e550cc4
ruirui.qu <ruirui.qu@qunar.com>
2018-06-15 10:49:31 +0800

    add

--- javacode/src/main/java/dubbo/prac/Dubbo.java
+++
@@ -1,7 +0,0 @@
-package dubbo.prac;
-
-//--------------------- Change Logs----------------------
-// <p>@author ruirui.qu Initial Created at 18/5/8<p>
-//-------------------------------------------------------
-public class Dubbo {
-}
--- javacode/src/main/java/dubbo/prac/service/ExampleDubbo.java
+++
@@ -1,9 +0,0 @@
-package dubbo.prac.service;
-
-// --------------------- Change Logs----------------------
-// <p>@author ruirui.qu Initial Created at 18/5/8<p>
-// -------------------------------------------------------
-public interface ExampleDubbo {
-
-    void sayHello();
-}
