09e8bce5efdf29eba06d4a310f408b88a186ce8d
ruirui.qu <ruirui.qu@qunar.com>
2018-06-15 17:49:59 +0800

    add

--- javacode/src/main/java/com/hua/algorithm/service/link/utils/MainTest.java
+++ javacode/src/main/java/com/hua/algorithm/service/link/utils/MainTest.java
@@ -22,8 +22,24 @@ public class MainTest {
         System.out.println("\n");
         System.out.println("删除单向链表倒数第"+4+"个节点==============");
         SingleLinkedUtils.delBackWardsKNode(singleLinked,4);
+
+        SingleLinked singleLinked1 = new SingleLinked();
+        singleLinked1.addTail(1);
+        singleLinked1.addTail(2);
+        singleLinked1.addTail(3);
+        singleLinked1.addTail(4);
+        System.out.println("\n");
+        System.out.println("删除单向链表倒数第"+2+"个节点==============");
+        SingleLinkedUtils.delBackWardsKNode(singleLinked1,2);
+
+
+        SingleLinked singleLinked3 = new SingleLinked();
+        singleLinked3.addTail(1);
+        singleLinked3.addTail(2);
+        singleLinked3.addTail(3);
+        singleLinked3.addTail(4);
         System.out.println("\n");
         System.out.println("反转单向链表==============");
-        SingleLinkedUtils.reverseLinkedByNSpace(singleLinked);
+        SingleLinkedUtils.reverseLinkedByNSpace(singleLinked3);
     }
 }
